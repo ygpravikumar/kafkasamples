584d8b4dfcbc0da8181e6ac13137fb3da676185d
Ravi G <ravig@Ravis-MacBook-Pro.local>
2023-09-25 10:50:59 +0530

    Add tests using mock server client

--- src/test/java/com/example/karate/PersonEventTest.java
+++ src/test/java/com/example/karate/PersonEventTest.java
@@ -7,22 +7,26 @@ import static org.awaitility.Awaitility.await;
 import com.example.domain.PersonDAO;
 import com.example.domain.PersonRepository;
 import com.example.pojo.Person;
+import io.restassured.RestAssured;
 import java.time.Duration;
 import java.util.ArrayList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockserver.client.MockServerClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.MockServerContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(
     properties = {
         "spring.kafka.consumer.auto-offset-reset=earliest",
@@ -38,9 +42,23 @@ class PersonEventTest {
       DockerImageName.parse("confluentinc/cp-kafka:7.3.3")
   );
 
+  @Container
+  static MockServerContainer mockServerContainer =
+      new MockServerContainer(DockerImageName.parse("mockserver/mockserver:5.15.0"));
+
+  static MockServerClient mockServerClient;
+
+  @LocalServerPort
+  private Integer port;
   @DynamicPropertySource
   static void overrideProperties(DynamicPropertyRegistry registry) {
     registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+
+    mockServerClient = new MockServerClient(
+        mockServerContainer.getHost(),
+        mockServerContainer.getServerPort()
+    );
+    registry.add("photos.api.base-url", mockServerContainer::getEndpoint);
   }
 
   @Autowired
@@ -55,6 +73,8 @@ class PersonEventTest {
     person.setFirstName("Ravi Kumar");
     person.setLastName("G");
     personRepository.save(person);
+    RestAssured.port = port;
+    mockServerClient.reset();
   }
 
   @Test
